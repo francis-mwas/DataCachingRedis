d147fa8ba585de38a6e30bcbf3f8fcd44e8317ac
francis-mwas <mwangif184@gmail.com>
2022-04-06 21:52:37 +0300

    Added redis caching in getting a blog by user id endpoint

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,22 +1,35 @@
 const mongoose = require('mongoose');
+const redis = require('redis');
+const util = require('util');
 const requireLogin = require('../middlewares/requireLogin');
 
 const Blog = mongoose.model('Blog');
 
-module.exports = app => {
+const redisUrl = 'redis://127.0.0.1:6379';
+
+module.exports = (app) => {
   app.get('/api/blogs/:id', requireLogin, async (req, res) => {
     const blog = await Blog.findOne({
       _user: req.user.id,
-      _id: req.params.id
+      _id: req.params.id,
     });
 
     res.send(blog);
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
+    const client = redis.createClient(redisUrl);
+    client.get = util.promisify(client.get);
+    const cachedBlogs = await client.get(req.user.id);
+    if (cachedBlogs) {
+      console.log('SERVING FROM CACHE');
+      return res.send(JSON.parse(cachedBlogs));
+    }
     const blogs = await Blog.find({ _user: req.user.id });
-
+    console.log('SERVING FROM MONGODB');
     res.send(blogs);
+
+    client.set(req.user.id, JSON.stringify(blogs));
   });
 
   app.post('/api/blogs', requireLogin, async (req, res) => {
@@ -25,7 +38,7 @@ module.exports = app => {
     const blog = new Blog({
       title,
       content,
-      _user: req.user.id
+      _user: req.user.id,
     });
 
     try {
