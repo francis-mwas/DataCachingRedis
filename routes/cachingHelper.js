e0363d7055691606f8312f855c8136243f64c8bd
francis-mwas <mwangif184@gmail.com>
2022-04-12 21:34:33 +0300

    Make caching reusable

---
+++ routes/cachingHelper.js
@@ -0,0 +1,12 @@
+Person.find({ occupation: /host/ })
+  .where('name.last')
+  .equals('Ghost')
+  .where('age')
+  .gt(17)
+  .lt(66)
+  .where('likes')
+  .in(['vaporizing', 'talking'])
+  .limit(10)
+  .sort('-occupation')
+  .select('name occupation')
+  .exec(callback);
