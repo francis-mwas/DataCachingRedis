7a1ad8dc9ec43462b2a3e8a25eb3a277f51b0a91
francis-mwas <mwangif184@gmail.com>
2022-04-21 23:18:43 +0300

    Testing the frontend with puppetteer

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,11 +1,26 @@
 const puppeteer = require('puppeteer');
+let browser, page;
 
+beforeEach(async () => {
+  browser = await puppeteer.launch({
+    headless: false,
+  });
+  page = await browser.newPage();
 
-test('Adds two numbers together', () => {
-  const sum = 1 + 5;
-  expect(sum).toEqual(6);
+  await page.goto('localhost:3000');
+});
+afterEach(async () => {
+  await browser.close();
 });
 
-test('launch the browser', ()=>{
-    const browser = await = puppeteer.launch({});
-})
+test('launch the browser', async () => {
+  const text = await page.$eval('a.brand-logo', (el) => el.innerHTML);
+
+  expect(text).toEqual('Blogster');
+});
+test('clicling login starts oauth flow', async()=>{
+  await page.click('.right a')
+  const url = await page.url();
+
+  console.log(url);
+});
