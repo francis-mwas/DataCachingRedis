642d1b754745bd975a25869233b58ce1e0039d98
francis-mwas <mwangif184@gmail.com>
2022-05-10 23:18:06 +0300

    fixed puppeteer error

--- tests/header.test.js
+++ tests/header.test.js
@@ -9,17 +9,17 @@ beforeEach(async () => {
 
   await page.goto('localhost:3000');
 });
-afterEach(async () => {
-  await browser.close();
-});
+// afterEach(async () => {
+//   await browser.close();
+// });
 
 test('launch the browser', async () => {
   const text = await page.$eval('a.brand-logo', (el) => el.innerHTML);
 
   expect(text).toEqual('Blogster');
 });
-test('clicling login starts oauth flow', async()=>{
-  await page.click('.right a')
+test('clicling login starts oauth flow', async () => {
+  await page.click('.right a');
   const url = await page.url();
 
   console.log(url);
